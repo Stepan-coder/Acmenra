450c35207f1a17d6c3cbbc35c8ab75aa6c3ff8fd
Stepan-coder <56876553+Stepan-coder@users.noreply.github.com>
2021-04-24 04:08:44 +0500

    Updating files

--- RandomForestClassifier.py
+++ RandomForestClassifier.py
@@ -5,10 +5,9 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import roc_auc_score
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 
-
-class DTreeClassifier:
+class RForestClassifier:
     def __init__(self, data_df, target, train_split, show=False):
         self.importance = {}
         self.is_grif_fit = False
@@ -24,15 +23,16 @@ class DTreeClassifier:
                                                                                 data_df[self.target],
                                                                                 train_size=self.train_split,
                                                                                 random_state=13)
-        self.types = {'max_depth': int,
+        self.types = {'n_estimators': int,
+                      'max_depth': int,
                       'max_features': str,
                       'min_samples_leaf': int,
                       'min_samples_split': int,
                       'criterion': str}
 
-    def fit_grid(self, params_dict=None, max_depth=None, max_features=None, min_samples_leaf=None,
+    def fit_grid(self, params_dict=None, n_estimators=None,  max_depth=None, max_features=None, min_samples_leaf=None,
                  min_samples_split=None, criterion=None, step=1, cross_validation=5):
-        print("Learning GridSearch DecisionTreeClassifier...")
+        print("Learning GridSearch RandomForestClassifier...")
         is_params_none = max_depth is None and max_features is None and min_samples_leaf is None and \
                          min_samples_split is None and criterion is None
         if params_dict is not None and is_params_none:
@@ -41,6 +41,9 @@ class DTreeClassifier:
                 self.check_param(param, params[param], self.types)
         elif params_dict is None and not is_params_none:
             params = {}
+            if n_estimators is not None:
+                self.check_param('n_estimators', n_estimators, self.types)
+                params['n_estimators'] = self.get_choosed_params(n_estimators, step)
             if max_depth is not None:
                 self.check_param('max_depth', max_depth, self.types)
                 params['max_depth'] = self.get_choosed_params(max_depth, step)
@@ -57,7 +60,8 @@ class DTreeClassifier:
                 self.check_param('criterion', criterion, self.types)
                 params['criterion'] = criterion
         elif params_dict is None and is_params_none:
-            params = {'max_depth': self.get_choosed_params([i for i in range(1, self.keys_len + 1)], step) + [None],
+            params = {'n_estimators': self.get_choosed_params([i * 10 for i in range(1, self.keys_len + 1)], step),
+                      'max_depth': self.get_choosed_params([i for i in range(1, self.keys_len + 1)], step) + [None],
                       'max_features': ['sqrt', 'auto', 'log2', None],
                       'min_samples_leaf': self.get_choosed_params([i for i in range(1, self.keys_len + 1)], step),
                       'min_samples_split': self.get_choosed_params([i for i in range(2, self.keys_len + 1)], step),
@@ -66,26 +70,28 @@ class DTreeClassifier:
             raise Exception("You should only choose one way to select hyperparameters!")
         if self.show:
             self.show_grid_params(params)
-        dtc = DecisionTreeClassifier(random_state=13)
-        grid = GridSearchCV(dtc, params, cv=cross_validation)
+        rfc = RandomForestClassifier(random_state=13)
+        grid = GridSearchCV(rfc, params, cv=cross_validation)
         grid.fit(self.X_train, self.Y_train)
         self.grid_best_params = grid.best_params_
         self.is_grif_fit = True
 
-    def fit(self, grid_params=False, params=None, max_depth=None, max_features=None, min_samples_leaf=None,
-                 min_samples_split=None, criterion=None):
-        print("Learning DecisionTreeClassifier...")
+    def fit(self, grid_params=False, params=None, n_estimators=None, max_depth=None, max_features=None,
+                  min_samples_leaf=None, min_samples_split=None, criterion=None):
+        print("Learned RandomForestClassifier...")
         is_params_none = max_depth is None and max_features is None and min_samples_leaf is None and \
                          min_samples_split is None and criterion is None
         if grid_params and is_params_none and params is None:
-            self.dt = DecisionTreeClassifier(max_depth=self.grid_best_params['max_depth'],
+            self.rf = RandomForestClassifier(n_estimators=self.grid_best_params['n_estimators'],
+                                             max_depth=self.grid_best_params['max_depth'],
                                              max_features=self.grid_best_params['max_features'],
                                              min_samples_leaf=self.grid_best_params['min_samples_leaf'],
                                              min_samples_split=self.grid_best_params['min_samples_split'],
                                              criterion=self.grid_best_params['criterion'],
                                              random_state=13)
         elif not grid_params and not is_params_none and params is None:
-            self.dt = DecisionTreeClassifier(max_depth=max_depth,
+            self.rf = RandomForestClassifier(n_estimators=n_estimators,
+                                             max_depth=max_depth,
                                              max_features=max_features,
                                              min_samples_leaf=min_samples_leaf,
                                              min_samples_split=min_samples_split,
@@ -95,54 +101,50 @@ class DTreeClassifier:
             for param in params:
                 if param not in self.types:
                     raise Exception('The parameter "{0}" not in params list'.format(param))
-            self.dt = DecisionTreeClassifier(max_depth=params['max_depth'],
+            self.rf = RandomForestClassifier(n_estimators=params['n_estimators'],
+                                             max_depth=params['max_depth'],
                                              max_features=params['max_features'],
                                              min_samples_leaf=params['min_samples_leaf'],
                                              min_samples_split=params['min_samples_split'],
                                              criterion=params['criterion'],
                                              random_state=13)
         elif not grid_params and is_params_none and params is None:
-            self.dt = DecisionTreeClassifier()
+            self.rf = RandomForestClassifier()
         else:
             raise Exception("You should only choose one way to select hyperparameters!")
-        self.dt.fit(self.X_train, self.Y_train)
+        self.rf.fit(self.X_train, self.Y_train)
         self.is_model_fit = True
 
-    def get_best_params(self, is_list=False):
+    def get_best_params(self):
         if self.is_grif_fit:
-            if is_list:
-                best_params = {}
-                for gbp in self.grid_best_params:
-                    best_params[gbp] = [self.grid_best_params[gbp]]
-                return best_params
-            else:
-                return self.grid_best_params
+            return self.grid_best_params
         else:
             raise Exception('At first you need to learn grid')
 
     def get_feature_importances(self):
         if not self.is_model_fit:
-            raise Exception('You haven"t trained the DecisionTreeClassifier yet')
-        for index in range(len(self.dt.feature_importances_)):
-            self.importance[self.keys[index]] = self.dt.feature_importances_[index]
+            raise Exception('You haven"t trained the RandomForestClassifier yet')
+        for index in range(len(self.rf.feature_importances_)):
+            self.importance[self.keys[index]] = self.rf.feature_importances_[index]
         return {k: v for k, v in sorted(self.importance.items(), key=lambda item: item[1], reverse=True)}
 
     def get_roc_auc_score(self):
         if not self.is_model_fit:
-            raise Exception('You haven"t trained the DecisionTreeClassifier yet')
-        return roc_auc_score(self.y_test, self.dt.predict(self.x_test))
+            raise Exception('You haven"t trained the RandomForestClassifier yet')
+        return roc_auc_score(self.y_test, self.rf.predict(self.x_test))
 
     def get_mean_squared_error(self):
         if not self.is_model_fit:
-            raise Exception('You haven"t trained the DecisionTreeClassifier yet')
-        return mean_squared_error(self.y_test, self.dt.predict(self.x_test))
+            raise Exception('You haven"t trained the RandomForestClassifier yet')
+        return mean_squared_error(self.y_test, self.rf.predict(self.x_test))
 
     def get_mean_absolute_error(self):
         if not self.is_model_fit:
-            raise Exception('You haven"t trained the DecisionTreeClassifier yet')
-        return mean_absolute_error(self.y_test, self.dt.predict(self.x_test))
+            raise Exception('You haven"t trained the RandomForestClassifier yet')
+        return mean_absolute_error(self.y_test, self.rf.predict(self.x_test))
 
     def show_grid_params(self, params):
+        print("Learning GridSearch RandomForestClassifier...")
         count_elements = []
         multiply = 1
         for param in params:
@@ -168,4 +170,3 @@ class DTreeClassifier:
                                     format(param, types[param], type(p)))
         else:
             raise Exception('The value of the "{0}" parameter must be a non-empty list'.format(param))
-
