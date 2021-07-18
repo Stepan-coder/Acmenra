b336d4b9a1c7dfd48c3e73aa61e7e235ca33dbb7
Stepan-coder <56876553+Stepan-coder@users.noreply.github.com>
2021-07-19 03:46:24 +0500

    Update DataSet.py

--- DataSet.py
+++ DataSet.py
@@ -1,24 +1,32 @@
 import pandas as pd
 
 # GROUPS['USAGE_AUDIO'] = GROUPS['USAGE_AUDIO'].to_string()
+from typing import Dict
+
+
 class DataSet:
     def __init__(self,
                  dataset: pd.DataFrame = None,
                  file_folder: str = None,
                  filename: str = None,
-                 delimiter: str = None):
+                 delimiter: str = None,
+                 show: bool = False):
         self.dataset = None  # Dataset in pd.DataFrame
         self.dataset_len = None  # Number of records in the dataset
         self.dataset_keys = None  # Column names in the dataset
         self.dataset_keys_count = None  # Number of columns in the dataset
-
+        self.show = True
         self.load_dataset(dataset=dataset,
                           file_folder=file_folder,
                           filename=filename,
                           delimiter=delimiter)
-        self.get_dataset_analytics()
+        # self.get_dataset_analytics()
 
-    def get_dataset_analytics(self, show: bool = False):
+    def get_dataset_analytics(self):
+        """
+        This method calculates column metrics
+        :return:
+        """
         dataset_analytics = {}
         if self.dataset is not None:
             for key in self.dataset_keys:
@@ -56,7 +64,6 @@ class DataSet:
     def update_dataset_base_info(self):
         """
         This method updates the basic information about the dataset
-        :return:
         """
         if self.dataset is not None:
             self.dataset_len = len(self.dataset)
@@ -85,32 +92,78 @@ class DataSet:
                                encoding=encoding,
                                delimiter=delimiter)
 
+    def set_field_types(self, new_fields_type: type = None, exception: Dict[str, type] = None):
+        """
+        This method converts column types
+        :param new_fields_type: New type of dataset columns (excluding exceptions)
+        :param exception: Fields that have a different type from the main dataset type
+        """
+        if new_fields_type is None and exception is None:
+            raise Exception("One of the parameters \'new_fields_type\' or \'exception\' must not be empty")
+        for field in self.dataset:
+            if field not in exception and new_fields_type is not None:
+                self.set_field_type(field_name=field,
+                                    new_field_type=new_fields_type)
+            elif field in exception:
+                self.set_field_type(field_name=field,
+                                    new_field_type=exception[field])
+
+    def set_field_type(self, field_name: str, new_field_type: type):
+        """
+        This method converts column type
+        :param field_name: The name of the column in which we want to change the type
+        :param new_field_type: Field type
+        """
+        if new_field_type != str and new_field_type != int and new_field_type != float:
+            raise Exception(f"'{new_field_type}' is an invalid data type for conversion. Valid types: int, float, str")
+        if field_name in self.dataset:
+            primary_type = type(self.dataset[field_name][0]).__name__
+            if new_field_type == float or new_field_type == int:
+                self.dataset[field_name] = self.dataset[field_name].replace(",", ".", regex=True)\
+                                                                   .replace(" ", "", regex=True)\
+                                                                   .replace(u'\xa0', u'', regex=True)\
+                                                                   .fillna(0)
+                self.dataset[field_name] = self.dataset[field_name].astype(float)
+                if new_field_type == int:
+                    self.dataset[field_name] = self.dataset[field_name].astype(int)
+            else:
+                self.dataset[field_name] = self.dataset[field_name].astype(new_field_type)
+            secondary_type = type(self.dataset[field_name][0]).__name__
+            if self.show:
+                print(f"Convert DataSet field \'{field_name}\': {primary_type} -> {secondary_type}")
+        else:
+            raise Exception("There is no such column in the presented dataset")
+
 
 class DataSetFieldAnalytics:
     def __init__(self,
                  column_name: str,
-                 values: list,
-                 ):
+                 values: list):
         self.column_name = column_name
         self.values = values
-        self.field_type = type(values[0])
+        self.field_type = type(values[0]).__name__
         self.count = len(values)
         self.count_unique = len(list(set(values)))
         self.nan_count = self.get_nan_count()
-        # print(isinstance(values[0], str))
-
-        if isinstance(values[0], int) or isinstance(values[0], float):
+        if self.field_type.startswith("int") or self.field_type == "float":
             self.min = min(values)
             self.max = max(values)
             self.average = sum(values) / self.count
 
     def __str__(self):
         text = f"DataSetField:\n"
-        text += f"*Name: \'{self.column_name}\'\n"
-        text += f"*Type: \'{self.field_type}\'\n"
-        text += f"*Count(Unique)[NaN values]: {self.count}({self.count_unique})[{self.nan_count}]\n"
-        if isinstance(self.values[0], int) or isinstance(self.values[0], float):
-            text += f"*Min/Average/Max: {self.min}/{self.average}/{self.max}\n"
+        text += f"  -Name: \'{self.column_name}\'\n"
+        text += f"  -Type: \'{self.field_type}\'\n"
+        text += f"  -Count: {self.count}\n"
+        text += f"  -Count unique: {self.count_unique}\n"
+        text += f"  -Count NaN: {self.nan_count}\n"
+        if self.field_type.startswith("int") or self.field_type == "float":
+            text += f"  Numerical indicators:\n"
+            text += f"      -Min: {self.min}\n"
+            text += f"      -Max: {self.max}\n"
+            text += f"      -Average: {round(self.average, 2)}\n"
+        # Допилить Статистические методы, но с "оригинальным" датасетом фильтрацию проводить нельзя, т.к. можно "отрезать" не то
+
         return text
 
     def get_nan_count(self):
