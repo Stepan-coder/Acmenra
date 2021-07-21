4b7982f63c3ada664836be654e39b3df34799df5
Stepan-coder <56876553+Stepan-coder@users.noreply.github.com>
2021-07-21 15:43:40 +0500

    new_commit

--- dataset/DataSet.py
+++ dataset/DataSet.py
@@ -3,31 +3,24 @@ import json
 import math
 import numpy as np
 import pandas as pd
+
 from typing import Dict
+from DataSetColumn import *
 
 
 class DataSet:
-    def __init__(self,
-                 dataset: pd.DataFrame = None,
-                 dataset_project_folder: str = None,
-                 csv_filename: str = None,
-                 delimiter: str = None,
-                 show: bool = False):
-        self.dataset = None  # Dataset in pd.DataFrame
-        self.dataset_project_folder = dataset_project_folder
-        self.csv_filename = csv_filename
-        self.delimiter = delimiter
+    def __init__(self, dataset_project_name:str, show: bool = False):
+        """
+
+        :param show:
+        """
+        self.dataset_project_name = dataset_project_name
         self.show = show
+        self.dataset = None  # Dataset in pd.DataFrame
         self.dataset_len = None  # Number of records in the dataset
         self.dataset_keys = None  # Column names in the dataset
         self.dataset_keys_count = None  # Number of columns in the dataset
         self.dataset_analytics = {}
-        if dataset is not None or dataset_project_folder is not None or csv_filename is not None:
-            self.load_dataset(dataset=dataset,
-                              dataset_project_folder=dataset_project_folder,
-                              csv_filename=csv_filename,
-                              delimiter=delimiter)
-        self.get_dataset_analytics(normal_distribution=False)
 
     def __len__(self):
         return len(self.dataset)
@@ -86,50 +79,48 @@ class DataSet:
             raise Exception("There is no such column in the presented dataset")
 
     def load_dataset(self,
-                     dataset: pd.DataFrame = None,
-                     dataset_project_folder: str = None,
-                     dataset_project_name: str = None,
-                     csv_filename: str = None,
-                     delimiter: str = None,
-                     encoding: str = 'utf-8'):
+                     dataset: pd.DataFrame):
         """
         This method loads the dataset into the DataSet class
         :param dataset: Explicitly specifying pd. DataFrame as a dataset
-        :param dataset_project_folder: The path to the .csv file
-        :param csv_filename: The name of the .csv file
+        """
+        self.dataset = dataset
+        self._update_dataset_base_info()
+
+    def load_csv_dataset(self,
+                         csv_file: str,
+                         delimiter: str,
+                         encoding: str = 'utf-8'):
+        """
+        This method loads the dataset into the DataSet class
+        :param csv_file: The name of the .csv file
         :param delimiter: Symbol-split in a .csv file
         :param encoding: Explicit indication of the .csv file encoding
         :return:
         """
-        if csv_filename is not None:
-            if not csv_filename.endswith(".csv"):
+        if csv_file is not None:  # Checking that the uploaded file has the .csv format
+            if not csv_file.endswith(".csv"):
                 raise Exception("The dataset format should be '.csv'")
-        if csv_filename is not None and delimiter is None:
+        if csv_file is not None and delimiter is None:  # Checking that a separator is specified
             raise Exception("When loading a dataset from a .csv file, you must specify a separator character!")
-        if (dataset is not None and dataset_project_name is not None) \
-                or (dataset is not None and csv_filename is not None) \
-                or (dataset_project_name is not None and dataset is not None) \
-                or (dataset_project_name is not None and csv_filename is not None and dataset is not None):
-            raise Exception("You need to select only one of the ways to load the dataset!")
-        if dataset_project_name is None and csv_filename is None and dataset is None:
-            raise Exception("Please choose one of the ways to download the dataset")
-
-        if dataset is not None:  # If pd.DataFrame was uploaded to us, we believe that this is the dataset
-            self.dataset = dataset
-        elif csv_filename is not None and delimiter is not None:  # Otherwise, we load the dataset from csv
-            self.dataset = self._read_from_csv(filename=csv_filename,
-                                               delimiter=delimiter,
-                                               encoding=encoding)
-        elif dataset_project_folder is not None and dataset_project_name is not None:  # Otherwise, we believe that the dataset will be handed over to us later
-            self.dataset = None
-            folder = ""
-            if dataset_project_folder is not None and dataset_project_folder != "":
-                folder = dataset_project_folder + "\\"
-            with open(f"{folder}{dataset_project_name}\\{dataset_project_name}.json", 'r') as jsonfile:
-                dataset_info = json.load(jsonfile)
-                jsonfile.close()
-                print(dataset_info)
+        self.dataset = self._read_from_csv(filename=csv_file,
+                                           delimiter=delimiter,
+                                           encoding=encoding)
         self._update_dataset_base_info()
+
+    def load_dataset_project(self,
+                             dataset_project_folder: str,
+                             json_config_filename: str):
+        """
+        This method loads the dataset into the DataSet class
+        :param dataset_project_folder: The path to the .csv file
+        :param json_config_filename:
+        """
+        self.dataset = None
+        with open(f"{dataset_project_folder}\\{json_config_filename}", 'r') as jsonfile:
+            dataset_info = json.load(jsonfile)
+            jsonfile.close()
+            print(dataset_info)
 
     def export(self,
                dataset_name: str,
@@ -180,173 +171,6 @@ class DataSet:
         return pd.read_csv(filename,
                            encoding=encoding,
                            delimiter=delimiter)
-
-
-class DataSetFieldAnalytics:
-    def __init__(self,
-                 column_name: str,
-                 values: list,
-                 normal_distribution: bool = False,
-                 rounding_factor: int = 2):
-        self.column_name = column_name
-        self.values = values
-        self.normal_distribution = normal_distribution
-        self.rounding_factor = rounding_factor
-        self.field_type = type(values[0]).__name__
-        self.count = len(values)
-        self.count_unique = len(list(set(values)))
-        self.nan_count = self._get_nan_count()
-        if self.field_type.startswith("int") or self.field_type == "float":
-            self.min = min(values)
-            self.max = max(values)
-            self.average = sum(values) / self.count
-            if self.normal_distribution:  # Exporting additional statistics
-                math_rasp = self._get_math_rasp()
-                self.math_moda = self._get_math_moda(math_rasp)
-                self.math_wait = self._get_math_wait(math_rasp)
-                self.math_dispersion = self._get_math_dispersion(math_rasp)
-                self.math_sigma = math.sqrt(self.math_dispersion)
-
-    def __str__(self):
-        """
-        Displaying information about the column
-        """
-        text = f"DataSetField: \'{self.column_name}\'\n"
-        text += f"  -Name: \'{self.column_name}\'\n"
-        text += f"  -Type: \'{self.field_type}\'\n"
-        text += f"  -Count: {self.count}\n"
-        text += f"  -Count unique: {self.count_unique}\n"
-        text += f"  -Count NaN: {self.nan_count}\n"
-        if self.field_type.startswith("int") or self.field_type == "float":
-            text += f"  Numerical indicators:\n"
-            text += f"      -Min: {self.min}\n"
-            text += f"      -Max: {self.max}\n"
-            text += f"      -Average: {round(self.average, self.rounding_factor)}\n"
-            if self.normal_distribution:
-                text += f"  Normal distribution:\n"
-                text += f"      -Moda: {self.math_moda}\n"
-                text += f"      -Wait: {round(self.math_wait, self.rounding_factor)}\n"
-                text += f"      -Dispersion: {round(self.math_dispersion, self.rounding_factor)}\n"
-                text += f"      -Sigma: {round(self.math_sigma, self.rounding_factor)}\n"
-                text += "      -Moda - 3 * Sigma: {0}\n".format(round(self.math_wait - 3 * self.math_sigma,
-                                                                      self.rounding_factor))
-                text += "      -Moda - 2 * Sigma: {0}\n".format(round(self.math_wait - 2 * self.math_sigma,
-                                                                      self.rounding_factor))
-                text += "      -Moda - 1 * Sigma: {0}\n".format(round(self.math_wait - 1 * self.math_sigma),
-                                                                self.rounding_factor)
-                text += "      -Moda + 1 * Sigma: {0}\n".format(round(self.math_wait + 1 * self.math_sigma),
-                                                                self.rounding_factor)
-                text += "      -Moda + 2 * Sigma: {0}\n".format(round(self.math_wait + 2 * self.math_sigma),
-                                                                self.rounding_factor)
-                text += "      -Moda + 3 * Sigma: {0}\n".format(round(self.math_wait + 3 * self.math_sigma),
-                                                                self.rounding_factor)
-        return text
-
-    def to_json(self) -> dict:
-        """
-        This method export DataSet statistics as json
-        :return: dict[column name, json statistic]
-        """
-        data = {"column_name": self.column_name,
-                "type": self.field_type,
-                "count": self.count,
-                "count_unique": self.count_unique,
-                "count_NaN": self.nan_count
-                }
-        if self.field_type.startswith("int") or self.field_type == "float":
-            data["numerical_indicators"] = {"min": self.min,
-                                            "max": self.max,
-                                            "average": self.average
-                                            }
-            if self.normal_distribution:
-                data["normal_distribution"] = {
-                    "moda": self.math_moda,
-                    "wait": round(self.math_wait, self.rounding_factor),
-                    "dispersion": round(self.math_dispersion, self.rounding_factor),
-                    "sigma": round(self.math_sigma, self.rounding_factor),
-                    "moda-3Xsigma": round(self.math_wait - 3 * self.math_sigma, self.rounding_factor),
-                    "moda-2Xsigma": round(self.math_wait - 2 * self.math_sigma, self.rounding_factor),
-                    "moda-1Xsigma": round(self.math_wait - 1 * self.math_sigma, self.rounding_factor),
-                    "moda+1Xsigma": round(self.math_wait + 1 * self.math_sigma, self.rounding_factor),
-                    "moda+2Xsigma": round(self.math_wait + 2 * self.math_sigma, self.rounding_factor),
-                    "moda+3Xsigma": round(self.math_wait + 3 * self.math_sigma, self.rounding_factor)
-
-                }
-        return {self.column_name: data}
-
-    def _get_nan_count(self) -> int:
-        """
-        This method calculate count of NaN values
-        :return: int
-        """
-        nan_cnt = 0
-        for value in self.values:
-            if pd.isna(value):
-                nan_cnt += 1
-        return nan_cnt
-
-    def _get_math_rasp(self):
-        """
-        This method calculates the frequency of list values as a percentage
-        :return: dict
-        """
-        math_rasp = {}
-        for value in self.values:
-            if value in list(math_rasp):
-                math_rasp[value] += (1 / len(self.values))
-            else:
-                math_rasp[value] = (1 / len(self.values))
-        return math_rasp
-
-    def _get_math_dispersion(self, math_rasp_dict: dict) -> float:
-        """
-        This method calculates the mathematical variance
-        :param math_rasp_dict: Dictionary of the frequency of values
-        :return: float
-        """
-        math_wait = self._get_math_wait(math_rasp_dict)
-        math_wait_x2 = math_wait * math_wait
-        math_wait_2 = self._get_math_wait_2(math_rasp_dict)
-        return math_wait_2 - math_wait_x2
-
-    @staticmethod
-    def _get_math_moda(math_rasp_dict: dict):
-        """
-        This method calculates the mathematical mode (the most frequent value)
-        :param math_rasp_dict: Dictionary of the frequency of values
-        :return:
-        """
-        moda = -10000000000
-        moda_key = -1
-        for key in math_rasp_dict:
-            if math_rasp_dict[key] > moda:
-                moda = math_rasp_dict[key]
-                moda_key = key
-        return moda_key
-
-    @staticmethod
-    def _get_math_wait(math_rasp_dict: dict) -> float:
-        """
-        This method calculates the mathematical expectation
-        :param math_rasp_dict: Dictionary of the frequency of values
-        :return: float
-        """
-        math_wait = 0
-        for md in math_rasp_dict:
-            math_wait += float(md) * math_rasp_dict[md]
-        return math_wait
-
-    @staticmethod
-    def _get_math_wait_2(math_rasp_dict: dict) -> float:
-        """
-        This method calculates the mathematical expectation squared
-        :param math_rasp_dict: Dictionary of the frequency of values
-        :return: float
-        """
-        math_wait_2 = 0
-        for md in math_rasp_dict:
-            math_wait_2 += float(md) * float(md) * math_rasp_dict[md]
-        return math_wait_2
 
 
 def merge_two_dicts(dict1, dict2):
---
+++ dataset/DataSetColumn.py
@@ -0,0 +1,168 @@
+import math
+
+
+class DataSetFieldAnalytics:
+    def __init__(self,
+                 column_name: str,
+                 values: list,
+                 normal_distribution: bool = False,
+                 rounding_factor: int = 2):
+        self.column_name = column_name
+        self.values = values
+        self.normal_distribution = normal_distribution
+        self.rounding_factor = rounding_factor
+        self.field_type = type(values[0]).__name__
+        self.count = len(values)
+        self.count_unique = len(list(set(values)))
+        self.nan_count = self._get_nan_count()
+        if self.field_type.startswith("int") or self.field_type == "float":
+            self.min = min(values)
+            self.max = max(values)
+            self.average = sum(values) / self.count
+            if self.normal_distribution:  # Exporting additional statistics
+                math_rasp = self._get_math_rasp()
+                self.math_moda = self._get_math_moda(math_rasp)
+                self.math_wait = self._get_math_wait(math_rasp)
+                self.math_dispersion = self._get_math_dispersion(math_rasp)
+                self.math_sigma = math.sqrt(self.math_dispersion)
+
+    def __str__(self):
+        """
+        Displaying information about the column
+        """
+        text = f"DataSetField: \'{self.column_name}\'\n"
+        text += f"  -Name: \'{self.column_name}\'\n"
+        text += f"  -Type: \'{self.field_type}\'\n"
+        text += f"  -Count: {self.count}\n"
+        text += f"  -Count unique: {self.count_unique}\n"
+        text += f"  -Count NaN: {self.nan_count}\n"
+        if self.field_type.startswith("int") or self.field_type == "float":
+            text += f"  Numerical indicators:\n"
+            text += f"      -Min: {self.min}\n"
+            text += f"      -Max: {self.max}\n"
+            text += f"      -Average: {round(self.average, self.rounding_factor)}\n"
+            if self.normal_distribution:
+                text += f"  Normal distribution:\n"
+                text += f"      -Moda: {self.math_moda}\n"
+                text += f"      -Wait: {round(self.math_wait, self.rounding_factor)}\n"
+                text += f"      -Dispersion: {round(self.math_dispersion, self.rounding_factor)}\n"
+                text += f"      -Sigma: {round(self.math_sigma, self.rounding_factor)}\n"
+                text += "      -Moda - 3 * Sigma: {0}\n".format(round(self.math_wait - 3 * self.math_sigma,
+                                                                      self.rounding_factor))
+                text += "      -Moda - 2 * Sigma: {0}\n".format(round(self.math_wait - 2 * self.math_sigma,
+                                                                      self.rounding_factor))
+                text += "      -Moda - 1 * Sigma: {0}\n".format(round(self.math_wait - 1 * self.math_sigma),
+                                                                self.rounding_factor)
+                text += "      -Moda + 1 * Sigma: {0}\n".format(round(self.math_wait + 1 * self.math_sigma),
+                                                                self.rounding_factor)
+                text += "      -Moda + 2 * Sigma: {0}\n".format(round(self.math_wait + 2 * self.math_sigma),
+                                                                self.rounding_factor)
+                text += "      -Moda + 3 * Sigma: {0}\n".format(round(self.math_wait + 3 * self.math_sigma),
+                                                                self.rounding_factor)
+        return text
+
+    def to_json(self) -> dict:
+        """
+        This method export DataSet statistics as json
+        :return: dict[column name, json statistic]
+        """
+        data = {"column_name": self.column_name,
+                "type": self.field_type,
+                "count": self.count,
+                "count_unique": self.count_unique,
+                "count_NaN": self.nan_count
+                }
+        if self.field_type.startswith("int") or self.field_type == "float":
+            data["numerical_indicators"] = {"min": self.min,
+                                            "max": self.max,
+                                            "average": self.average
+                                            }
+            if self.normal_distribution:
+                data["normal_distribution"] = {
+                    "moda": self.math_moda,
+                    "wait": round(self.math_wait, self.rounding_factor),
+                    "dispersion": round(self.math_dispersion, self.rounding_factor),
+                    "sigma": round(self.math_sigma, self.rounding_factor),
+                    "moda-3Xsigma": round(self.math_wait - 3 * self.math_sigma, self.rounding_factor),
+                    "moda-2Xsigma": round(self.math_wait - 2 * self.math_sigma, self.rounding_factor),
+                    "moda-1Xsigma": round(self.math_wait - 1 * self.math_sigma, self.rounding_factor),
+                    "moda+1Xsigma": round(self.math_wait + 1 * self.math_sigma, self.rounding_factor),
+                    "moda+2Xsigma": round(self.math_wait + 2 * self.math_sigma, self.rounding_factor),
+                    "moda+3Xsigma": round(self.math_wait + 3 * self.math_sigma, self.rounding_factor)
+
+                }
+        return {self.column_name: data}
+
+    def _get_nan_count(self) -> int:
+        """
+        This method calculate count of NaN values
+        :return: int
+        """
+        nan_cnt = 0
+        for value in self.values:
+            if pd.isna(value):
+                nan_cnt += 1
+        return nan_cnt
+
+    def _get_math_rasp(self):
+        """
+        This method calculates the frequency of list values as a percentage
+        :return: dict
+        """
+        math_rasp = {}
+        for value in self.values:
+            if value in list(math_rasp):
+                math_rasp[value] += (1 / len(self.values))
+            else:
+                math_rasp[value] = (1 / len(self.values))
+        return math_rasp
+
+    def _get_math_dispersion(self, math_rasp_dict: dict) -> float:
+        """
+        This method calculates the mathematical variance
+        :param math_rasp_dict: Dictionary of the frequency of values
+        :return: float
+        """
+        math_wait = self._get_math_wait(math_rasp_dict)
+        math_wait_x2 = math_wait * math_wait
+        math_wait_2 = self._get_math_wait_2(math_rasp_dict)
+        return math_wait_2 - math_wait_x2
+
+    @staticmethod
+    def _get_math_moda(math_rasp_dict: dict):
+        """
+        This method calculates the mathematical mode (the most frequent value)
+        :param math_rasp_dict: Dictionary of the frequency of values
+        :return:
+        """
+        moda = -10000000000
+        moda_key = -1
+        for key in math_rasp_dict:
+            if math_rasp_dict[key] > moda:
+                moda = math_rasp_dict[key]
+                moda_key = key
+        return moda_key
+
+    @staticmethod
+    def _get_math_wait(math_rasp_dict: dict) -> float:
+        """
+        This method calculates the mathematical expectation
+        :param math_rasp_dict: Dictionary of the frequency of values
+        :return: float
+        """
+        math_wait = 0
+        for md in math_rasp_dict:
+            math_wait += float(md) * math_rasp_dict[md]
+        return math_wait
+
+    @staticmethod
+    def _get_math_wait_2(math_rasp_dict: dict) -> float:
+        """
+        This method calculates the mathematical expectation squared
+        :param math_rasp_dict: Dictionary of the frequency of values
+        :return: float
+        """
+        math_wait_2 = 0
+        for md in math_rasp_dict:
+            math_wait_2 += float(md) * float(md) * math_rasp_dict[md]
+        return math_wait_2
