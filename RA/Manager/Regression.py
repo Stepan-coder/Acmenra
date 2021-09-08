2676b9976feaa42fcf7f38f32de8748d1bfc9a4a
Stepan-coder <56876553+Stepan-coder@users.noreply.github.com>
2021-09-09 01:35:45 +0500

    new_commit

--- RA/Manager/Regression.py
+++ RA/Manager/Regression.py
@@ -6,6 +6,7 @@ from RA.models.Regression.LinRegression import *
 from RA.models.Regression.DTRegressor import *
 from RA.models.Regression.RFRegressor import *
 from RA.models.Regression.ETRegressor import *
+from RA.models.Regression.PasAggrRegressor import *
 from RA.models.Regression.GBRegressor import *
 from RA.models.Regression.SGDRegressor import *
 from RA.models.Regression.LassoRegressor import *
@@ -21,59 +22,60 @@ from RA.models.Regression.HuberRegressor import *
 from RA.models.Regression.KNeigRegressor import *
 from RA.models.Regression.SVRegressor import *
 from RA.models.Regression.LinSVRegressor import *
+from RA.models.Regression.GaussPRegressor import *
+from RA.models.Regression.IsoRegression import *
+from RA.models.Regression.ARDRegression import *
+from RA.models.Regression.TSenRegressor import *
 from RA.models.Regression.ABoostRegressor import *
 from RA.models.Regression.BagRegressor import *
+from RA.models.Regression.MLPRegressor import *
+from RA.models.Regression.RadNeigRegressor import *
+from RA.models.Regression.RANSACRegressor import *
+
 
 # All regressions
-# [<class 'sklearn.ensemble.forest.RandomForestRegressor'>,
-# <class 'sklearn.ensemble.forest.ExtraTreesRegressor'>,
-# <class 'sklearn.ensemble.bagging.BaggingRegressor'>,
-# <class 'sklearn.ensemble.gradient_boosting.GradientBoostingRegressor'>,
-# <class 'sklearn.ensemble.weight_boosting.AdaBoostRegressor'>,
-# <class 'sklearn.gaussian_process.gpr.GaussianProcessRegressor'>,
-# <class 'sklearn.isotonic.IsotonicRegression'>,
-# <class 'sklearn.linear_model.bayes.ARDRegression'>,
-# <class 'sklearn.linear_model.huber.HuberRegressor'>,
-# <class 'sklearn.linear_model.base.LinearRegression'>,
-# <class 'sklearn.linear_model.logistic.LogisticRegression'>,
-# <class 'sklearn.linear_model.logistic.LogisticRegressionCV'>,
-# <class 'sklearn.linear_model.passive_aggressive.PassiveAggressiveRegressor'>,
-# <class 'sklearn.linear_model.randomized_l1.RandomizedLogisticRegression'>,
-# <class 'sklearn.linear_model.stochastic_gradient.SGDRegressor'>, <class
-# 'sklearn.linear_model.theil_sen.TheilSenRegressor'>, <class
-# 'sklearn.linear_model.ransac.RANSACRegressor'>, <class
-# 'sklearn.multioutput.MultiOutputRegressor'>, <class
-# 'sklearn.neighbors.regression.KNeighborsRegressor'>, <class
-# 'sklearn.neighbors.regression.RadiusNeighborsRegressor'>, <class
-# 'sklearn.neural_network.multilayer_perceptron.MLPRegressor'>, <class
-# 'sklearn.tree.tree.DecisionTreeRegressor'>, <class
-# 'sklearn.tree.tree.ExtraTreeRegressor'>, <class
-# 'sklearn.svm.classes.SVR'>]
+# <class 'sklearn.linear_model.ransac.RANSACRegressor'>
 
 
 class Regression:
     def __init__(self):
-        self.__models = {"LinearRegression": LinRegressor(),
+        self.__models = {"AdaBoostRegressor": ABoostRegressor(),
+                         "ARDRegression": ARDRegression(),
+                         "BaggingRegressor": BagRegressor(),
+
+                         "BayesRidgeRegressor": BayesRidgeRegressor(),
                          "DecisionTreeRegressor": DTRegressor(),
-                         "RandomForestRegressor": RFRegressor(),
-                         "ExtraTreesRegressor": ETRegressor(),
-                         "GradientBoostingRegressor": GBRegressor(),
-                         "StochasticGradientDescentRegressor": SGDRegressor(),
-                         "LassoRegressor": LassoRegressor(),
-                         "LassoCVRegressor": LassoCVRegressor(),
-                         "RidgeRegressor": RidgeRegressor(),
-                         "RidgeCVRegressor": RidgeCVRegressor(),
-                         "BayesianRidgeRegressor": BayesianRidgeRegressor(),
-                         "ElasticNetRegressor": ENetRegressor(),
                          "ElasticNetCVRegressor": ENetCVRegressor(),
-                         "LarsRegressor": LarsRegressor(),
-                         "LarsCVRegressor": LarsCVRegressor(),
+
+                         "ElasticNetRegressor": ENetRegressor(),
+                         "ExtraTreesRegressor": ETRegressor(),
+                         "GaussianProcessRegressor": GaussPRegressor(),
+
+                         "GradientBoostingRegressor": GBRegressor(),
                          "HuberRegressor": HuberRRegressor(),
+                         "IsoRegression": IsoRegression(),
+
                          "KNeighborsRegressor": KNRegressor(),
-                         "SVRegressor": SVRegressor(),
+                         "LarsCVRegressor": LarsCVRegressor(),
+                         "LarsRegressor": LarsRegressor(),
+
+                         "LassoCVRegressor": LassoCVRegressor(),
+                         "LassoRegressor": LassoRegressor(),
+                         "LinearRegression": LinRegressor(),
+
                          "LinSVRegressor": LinSVRegressor(),
-                         "ABoostRegressor": ABoostRegressor(),
-                         "BagRegressor": BagRegressor()}
+                         "MultiLayerPerceptronRegressor": MLPRegressor(),
+                         "PassiveAggressiveRegressor": PasAggrRegressor(),
+
+                         "RadiusNeighborsRegressor": RadNeigRegressor(),
+                         "RANSACRegressor": RANSACRegressor(),
+                         "RandomForestRegressor": RFRegressor(),
+                         "RidgeCVRegressor": RidgeCVRegressor(),
+
+                         "RidgeRegressor": RidgeRegressor(),
+                         "StochasticGradientDescentRegressor": SGDRegressor(),
+                         "SVRegressor": SVRegressor(),
+                         "TSenRegressor": TSenRegressor()}
 
     def get_model(self, model_name: str):
         if model_name not in self.__models:
@@ -127,28 +129,34 @@ class Regression:
                                                                     train_size=train_split, random_state=13)
         simple_test = self.__blitz_test(X_train=X_train, x_test=x_test,
                                         Y_train=Y_train, y_test=y_test,
+                                        task=task, target=target,
                                         converter="Simple", prefit=prefit, n_jobs=n_jobs)
         SS_test = self.__blitz_test(X_train=SS_X_train, x_test=SS_x_test,
                                     Y_train=SS_Y_train, y_test=SS_y_test,
+                                    task=task, target=target,
                                     converter="StandardScaler", prefit=prefit, n_jobs=n_jobs)
         MMS_test = self.__blitz_test(X_train=MMS_X_train, x_test=MMS_x_test,
                                      Y_train=MMS_Y_train, y_test=MMS_y_test,
+                                     task=task, target=target,
                                      converter="MinMaxScaler", prefit=prefit, n_jobs=n_jobs)
         RS_test = self.__blitz_test(X_train=RS_X_train, x_test=RS_x_test,
                                     Y_train=RS_Y_train, y_test=RS_y_test,
+                                    task=task, target=target,
                                     converter="RobustScaler", prefit=prefit, n_jobs=n_jobs)
         MAS_test = self.__blitz_test(X_train=MAS_X_train, x_test=MAS_x_test,
                                      Y_train=MAS_Y_train, y_test=MAS_y_test,
+                                     task=task, target=target,
                                      converter="MaxAbsScaler", prefit=prefit, n_jobs=n_jobs)
         N_test = self.__blitz_test(X_train=N_X_train, x_test=N_x_test,
                                    Y_train=N_Y_train, y_test=N_y_test,
+                                   task=task, target=target,
                                    converter="Normalizer", prefit=prefit, n_jobs=n_jobs)
         results = simple_test + SS_test + MMS_test + RS_test + MAS_test + N_test
         results.sort(key=itemgetter(2, 3, 4, 5, 6, 7),
                      reverse=False)
         if show:
             table = PrettyTable()
-            table.title = f"Regression model results"
+            table.title = f"Regression results X {len(results)} models"
             table.field_names = ["Model", "Converter",
                                  "ROC AUC Score", "R-Squared Error", "Mean Absolute Error",
                                  "Mean Squared Error", "Root Mean Squared Error", "Median Absolute Error"]
@@ -164,25 +172,22 @@ class Regression:
                      converter: str, prefit: bool = False, n_jobs: int = 1):
         results = []
         for model in self.__models:
-            # try:
-            this_model = self.__models[model].copy()
-            this_model.set_params(task=task, target=target, train_split=int(len(task) * 0.5))
-            this_model.set_train_test(X_train=X_train, x_test=x_test,
-                                      Y_train=Y_train, y_test=y_test)
-            if prefit:
-                this_model.fit_grid(count=0, grid_n_jobs=n_jobs)
-                this_model.fit(grid_params=True, n_jobs=n_jobs)
-            else:
-                this_model.fit(n_jobs=n_jobs)
-            results.append([model, converter,
-                            this_model.get_roc_auc_score(), this_model.get_r_squared_error(),
-                            this_model.get_mean_absolute_error(), this_model.get_mean_squared_error(),
-                            this_model.get_root_mean_squared_error(), this_model.get_median_absolute_error()])
-            # except:
-            #     results.append([model, converter,
-            #                     float("inf"), float("inf"), float("inf"),
-            #                     float("inf"), float("inf"), float("inf")])
+            try:
+                this_model = self.__models[model].copy()
+                this_model.set_params(task=task, target=target, train_split=int(len(task) * 0.5))
+                this_model.set_train_test(X_train=X_train, x_test=x_test,
+                                          Y_train=Y_train, y_test=y_test)
+                if prefit:
+                    this_model.fit_grid(count=0, grid_n_jobs=n_jobs)
+                    this_model.fit(grid_params=True, n_jobs=n_jobs)
+                else:
+                    this_model.fit(n_jobs=n_jobs)
+                results.append([model, converter,
+                                this_model.get_roc_auc_score(), this_model.get_r_squared_error(),
+                                this_model.get_mean_absolute_error(), this_model.get_mean_squared_error(),
+                                this_model.get_root_mean_squared_error(), this_model.get_median_absolute_error()])
+            except:
+                results.append([model, converter,
+                                float("inf"), float("inf"), float("inf"),
+                                float("inf"), float("inf"), float("inf")])
         return results
-
-
-
