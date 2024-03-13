da0e4c71c5e6dbe7262c897069c20b13cf3f5fa9
St.Dev.Lab <56876553+Stepan-coder@users.noreply.github.com>
2024-03-13 15:30:19 +0500

    Update HGBClassifier.py

--- RA/models/Classification/HGBClassifier.py
+++ RA/models/Classification/HGBClassifier.py
@@ -81,7 +81,6 @@ class HGBClassifier:
         """
         This method sets the parameters for the training grid
         :param count: The number of elements in the grid
-        :return: None
         """
         self.__default = {"loss": Param(ptype=[str],
                                         def_val='auto',
@@ -305,13 +304,11 @@ class HGBClassifier:
 
     def get_text_name(self) -> str:
         """
-        :return: This method returns the text-name of current model
         """
         return self.__text_name
 
     def get_grid_locked_params(self) -> dict:
         """
-        :return: This method returns a dictionary of "locked" parameters
         """
         if not self.__is_grid_fit:
             raise Exception('At first you need to learn grid')
@@ -323,19 +320,16 @@ class HGBClassifier:
 
     def get_locked_params_names(self) -> List[str]:
         """
-        :return: This method return the list of locked params
         """
         return [p for p in self.__default if self.__default[p].is_locked]
 
     def get_non_locked_params_names(self) -> List[str]:
         """
-        :return: This method return the list of non locked params
         """
         return [p for p in self.__default if not self.__default[p].is_locked]
 
     def get_default_param_types(self) -> dict:
         """
-        :return: This method return default model param types
         """
         default_param_types = {}
         for default in self.__default:
@@ -344,7 +338,6 @@ class HGBClassifier:
 
     def get_default_param_values(self) -> dict:
         """
-        :return: This method return default model param values
         """
         default_param_values = {}
         for default in self.__default:
@@ -353,7 +346,6 @@ class HGBClassifier:
 
     def get_default_grid_param_values(self) -> dict:
         """
-        :return: This method return default model param values for grid search
         """
         default_param_values = {}
         for default in self.__default:
@@ -363,28 +355,24 @@ class HGBClassifier:
     def get_is_model_fit(self) -> bool:
         """
         This method return flag is_model_fit
-        :return: is_model_fit
         """
         return self.__is_model_fit
 
     def get_is_grid_fit(self) -> bool:
         """
         This method return flag get_is_grid_fit
-        :return: get_is_grid_fit
         """
         return self.__is_grid_fit
 
     def get_is_dataset_set(self) -> bool:
         """
         This method return flag is_dataset_set
-        :return: is_dataset_set
         """
         return self.__is_dataset_set
 
     def get_grid_best_params(self) -> dict:
         """
         This method return the dict of best params for this model
-        :return: dict of best params for this model
         """
         if self.__is_grid_fit:
             return self.__grid_best_params
@@ -395,7 +383,6 @@ class HGBClassifier:
         """
         This method return dict of feature importance where key is the column of input dataset, and value is importance
         of this column
-        :return: dict of column importance
         """
         if not self.__is_model_fit:
             raise Exception(f"You haven't trained the {self.__text_name} yet!")
@@ -406,14 +393,12 @@ class HGBClassifier:
     def copy(self):
         """
         This method return copy of this class
-        :return: copy of this class
         """
         return copy.copy(self)
 
     def get_roc_auc_score(self) -> float:
         """
         This method calculates the "ROC AUC score" for the {self.__text_name} on the test data
-        :return: ROC AUC Score
         """
         error = float("inf")
         if not self.__is_model_fit:
@@ -427,7 +412,6 @@ class HGBClassifier:
     def get_r_squared_error(self) -> float:
         """
         This method calculates the "R-Squared_error" for the on the test data
-        :return: R-Squared_error
         """
         error = float("inf")
         if not self.__is_model_fit:
@@ -441,7 +425,6 @@ class HGBClassifier:
     def get_mean_absolute_error(self) -> float:
         """
         This method calculates the "mean_absolute_error" for the {self.text_name} on the test data
-        :return: Mean Absolute Error
         """
         error = float("inf")
         if not self.__is_model_fit:
@@ -455,7 +438,6 @@ class HGBClassifier:
     def get_mean_squared_error(self) -> float:
         """
         This method calculates the "mean_squared_error" for the {self.text_name} on the test data
-        :return: Mean Squared Error
         """
         error = float("inf")
         if not self.__is_model_fit:
@@ -469,7 +451,6 @@ class HGBClassifier:
     def get_root_mean_squared_error(self) -> float:
         """
         This method calculates the "root_mean_squared_error" for the {self.text_name} on the test data
-        :return: Root Mean Squared Error
         """
         error = float("inf")
         if not self.__is_model_fit:
@@ -483,7 +464,6 @@ class HGBClassifier:
     def get_median_absolute_error(self) -> float:
         """
         This method calculates the "mean_squared_error" for the {self.text_name} on the test data
-        :return: Median Absolute Error
         """
         error = float("inf")
         if not self.__is_model_fit:
@@ -524,7 +504,6 @@ class HGBClassifier:
         :param Y_train: Training value sample
         :param x_test: Test data sample
         :param y_test: Test value sample
-        :return: None
         """
         self.__X_train, self.__x_test, self.__Y_train, self.__y_test = X_train, x_test, Y_train, y_test
         self.__is_dataset_set = True
@@ -532,7 +511,6 @@ class HGBClassifier:
     def __get_default_model_fit_time(self) -> float:
         """
         This method return time of fit model with defualt params
-        :return: time of fit model with defualt params
         """
         time_start = time.time()
         model = HistGradientBoostingClassifier(random_state=13)
